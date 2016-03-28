3c087dda71abf586a3bd78fb0aad88c0bb26eb17
mattsedlar <sedlarm@gmail.com>
2016-03-28 11:26:53 -0700

    looping through feed entries, introducing writing multiple objects to file

--- parseFeed.py
+++ parseFeed.py
@@ -6,33 +6,40 @@ import textwrap
 
 f = feedparser.parse('http://cepr.net/publications/reports/feed/rss')
 
-feed_date = f.updated[:-13]
+# can't use feed build date, so looping through entries for current reports
+# FOR TESTING PURPOSES
+today = "3-24-2016"
+# today = time.strftime('%m-%d-%Y')
+# convert month to absolute value to compare to feed dates
+# today = str(abs(int(today[0:2]))) + today[2:]
 
-if feed_date == time.strftime('%a, %d %b %Y'):
-    # grab pubDate
-    pubDate = str(f.entries[0].published_parsed.tm_year) + "-" + str(f.entries[0].published_parsed.tm_mon)
-    # grab title
-    title = f.entries[0].title
-    # grab author names
-    authors = f.entries[0].author
-    # find names between parentheses
-    m = re.search('\(([^)]+)\)', authors)
-    # remove 'and'
-    authors_string = m.group(1).replace("and", "")
-    # convert to list
-    authors_list = authors_string.split(',')
-    # abstract
-    # NOTE THIS IS NOT PULLING THE ACTUAL DESCRIPTION - NEEDS WORK
-    abstract = f.entries[0].description
-
-    # use length of list to loop and create individual author-name entries
-
-    # details
-    entry_head = textwrap.dedent("""\
-                Template-Type: ReDif-Paper 1.0
-                Handle: RePEc:epo:papers:"""+pubDate)
-
-# append details to file
-
-with open(time.strftime("%Y") + "-test.rdf", "a") as repec_file:
-     repec_file.write(entry_head)
+# BEGIN LOOP
+for x in f.entries:
+    entry_date = str(x.published_parsed.tm_mon) + "-" + str(x.published_parsed.tm_mday) + "-" + str(x.published_parsed.tm_year)
+    if entry_date == today:
+        # grab pubDate
+        pubDate = str(x.published_parsed.tm_year) + "-" + str(x.published_parsed.tm_mon)
+        # grab title
+        title = x.title
+        # grab author names
+        authors = x.author
+        # find names between parentheses
+        m = re.search('\(([^)]+)\)', authors)
+        # remove 'and'
+        authors_string = m.group(1).replace("and", ",")
+        # convert to list
+        authors_list = authors_string.split(',')
+        # abstract
+        # NOTE THIS IS NOT PULLING THE ACTUAL DESCRIPTION - NEEDS WORK
+        abstract = x.description
+        # use length of list to loop and create individual author-name entries
+        #details
+        entry_head = textwrap.dedent("""\
+            \n
+            Template-Type: ReDif-Paper 1.0
+            Handle: RePEc:epo:papers:""" + pubDate)
+        # append details to file
+        with open(time.strftime("%Y") + "-test.rdf", "a") as repec_file:
+            repec_file.write(entry_head)
+            # need for loop here
+            repec_file.write("\nAuthor-Name:" + authors_list[0])
